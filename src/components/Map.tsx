5e7cac221eeb4834330f0328f65129c1b5c7954b
micorix <michal.oreziak@gmail.com>
2020-06-08 23:42:18 +0200

    Use ctrl+scroll to zoom the map :children_crossing:

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -7,10 +7,13 @@ import React, {
   useRef,
 } from 'react';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-gesture-handling/dist/leaflet-gesture-handling.css';
 import L from 'leaflet';
+import { GestureHandling } from "leaflet-gesture-handling";
 import { defaultMarker } from '../utils/map';
 
 L.Marker.prototype.options.icon = defaultMarker;
+L.Map.addInitHook("addHandler", "gestureHandling", GestureHandling);
 
 const MapWrapper = styled.div`
   width: 100%;
@@ -38,7 +41,9 @@ const Map: FC<MapProps> = props => {
   const map = useRef<any>(null);
 
   useEffect(() => {
-    map.current = L.map(mapEl.current as HTMLDivElement);
+    map.current = L.map(mapEl.current as HTMLDivElement, {
+      gestureHandling: true
+    } as any);
     const defaultTileLayer = L.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       {
