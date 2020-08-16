a91f28d108a9a9884363e45c5fa653bc38d6a50b
Michał Oręziak <micorix.dev@gmail.com>
2020-08-16 23:28:42 +0200

    Read env vars from environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+import { environment as devEnvironment } from './environment.dev';
+import { environment as prodEnvironment } from './environment.prod';
+
+export interface Environment {
+  API_URL: string;
+  PUBLIC_SENTRY_DSN: string;
+}
+
+export const isProduction = process.env.NODE_ENV === 'production';
+
+export const environment: Environment = isProduction
+  ? prodEnvironment
+  : devEnvironment;
