9694cb090ead565cc8b8f5c741a39994d021ad52
Michał Oręziak <micorix.dev@gmail.com>
2020-10-14 18:37:16 +0200

    Add error handling for 5xx errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import * as Sentry from '@sentry/react';
 import {ThemeProvider} from 'emotion-theming';
 import {Global} from '@emotion/core';
+import {SWRConfig} from "swr";
+import handleError from "./api/handleError";
 import theme from './styling/theme';
 import Routes from './Routes';
 import {globalStyles} from './styling/globalStyles';
+
+const swrConfig = {
+    onError: handleError
+}
 
 const App: React.FC = () => {
     return (
         <Sentry.ErrorBoundary fallback={"An error has occurred"}>
             <ThemeProvider theme={theme}>
                 <Global styles={globalStyles}/>
-                <Routes/>
+                <SWRConfig value={swrConfig}>
+                    <Routes/>
+                </SWRConfig>
             </ThemeProvider>
         </Sentry.ErrorBoundary>
     );
--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,15 +1,10 @@
 import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 import { environment, isProduction } from '../environments/environment';
 
 export const setupSentry = () => {
   if (isProduction && environment.PUBLIC_SENTRY_DSN)
     Sentry.init({
-      dsn: environment.PUBLIC_SENTRY_DSN,
-      integrations: [
-        new Integrations.BrowserTracing(),
-      ],
-      tracesSampleRate: 1.0,
+      dsn: environment.PUBLIC_SENTRY_DSN
     });
 
 };
