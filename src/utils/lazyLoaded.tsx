4170dbadb9fae7e83252e5707242e313c816fa47
Maria Rukat <maria.rukat@nask.pl>
2021-01-08 12:38:56 +0100

    Fix type in lazyLoaded function

--- src/utils/lazyLoaded.tsx
+++ src/utils/lazyLoaded.tsx
@@ -1,7 +1,7 @@
 import React, { ComponentType, LazyExoticComponent, Suspense } from 'react';
 import Loader from '../components/Loader';
 
-const lazyLoaded = (Component: LazyExoticComponent<ComponentType<any>>) => {
+const lazyLoaded = (Component: LazyExoticComponent<ComponentType<any>>): ComponentType => {
   return (props: any) => (
     <Suspense fallback={<Loader />}>
       <Component {...props} />
