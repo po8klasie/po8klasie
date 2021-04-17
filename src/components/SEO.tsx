c41981b4d2be9f5358d7b67ad1fa4d80506162b9
Michał Oręziak <micorix.dev@gmail.com>
2021-04-17 17:33:58 +0200

    Remove SITE_ENV var. Remove most of conditional logic from SEO component

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,13 +1,12 @@
 import React, { FC } from 'react';
 import { Helmet } from 'react-helmet';
 import { useLocation } from '@reach/router';
-import { environment } from '../environments/environment';
 
+// when updating, please adjust meta tags in index.html accordingly
 const defaultTitle = 'WarsawLO';
 const titleTemplate = '%s · WarsawLO';
-const defaultDescription = 'Najprostsza i najszybsza wyszukiwarka warszawskich szkół średnich.';
+const defaultDescription = 'Najprostsza i najszybsza wyszukiwarka warszawskich szkół średnich';
 const twitterUsername = 'warsawlo';
-const siteUrl = environment.SITE_URL;
 
 interface SEOProps {
   title: string;
@@ -16,24 +15,24 @@ interface SEOProps {
 }
 
 const SEO: FC<SEOProps> = ({ title, description, isArticle }) => {
-  const { pathname } = useLocation();
+  const { href } = useLocation();
 
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    url: `${siteUrl}${pathname}`,
+    url: href,
   };
 
   return (
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
       <meta name="description" content={seo.description} />
-      {seo.url && <meta property="og:url" content={seo.url} />}
-      {(isArticle ? true : null) && <meta property="og:type" content="article" />}
-      {seo.title && <meta property="og:title" content={seo.title} />}
-      {seo.description && <meta property="og:description" content={seo.description} />}
-      {twitterUsername && <meta name="twitter:creator" content={twitterUsername} />}
-      {seo.title && <meta name="twitter:title" content={seo.title} />}
-      {seo.description && <meta name="twitter:description" content={seo.description} />}
+      <meta property="og:url" content={seo.url} />
+      {isArticle && <meta property="og:type" content="article" />}
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
     </Helmet>
   );
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 interface Environment {
   API_URL: string;
   PUBLIC_SENTRY_DSN: string;
-  SITE_URL: string;
 }
 
 const prodEnvironment: Environment = (window as any).config;
@@ -9,7 +8,6 @@ const prodEnvironment: Environment = (window as any).config;
 const devEnvironment: Environment = {
   API_URL: process.env.REACT_APP_API_URL as string,
   PUBLIC_SENTRY_DSN: '',
-  SITE_URL: `http://${window.location.hostname}`,
 };
 
 export const isProduction = process.env.NODE_ENV === 'production';
