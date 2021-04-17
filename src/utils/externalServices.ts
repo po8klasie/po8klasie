6a0594483e5e26667b794e166ffc67e65ab3f3bc
Michał Oręziak <micorix.dev@gmail.com>
2021-04-17 16:47:28 +0200

    Added env vars to entrypoint & index.html. Add isEnvVarEmpty function.

--- src/utils/externalServices.ts
+++ src/utils/externalServices.ts
@@ -1,11 +1,11 @@
 import * as Sentry from '@sentry/react';
 import { createInstance } from '@datapunt/matomo-tracker-react';
-import { environment, isProduction } from '../environments/environment';
+import { environment, isEnvVarEmpty, isProduction } from '../environments/environment';
 
 const { PUBLIC_SENTRY_DSN, MATOMO_BASE_URL, MATOMO_SITE_ID } = environment;
 
 export const setupSentry = (): void => {
-  if (isProduction && PUBLIC_SENTRY_DSN)
+  if (isProduction && !isEnvVarEmpty('PUBLIC_SENTRY_DSN'))
     Sentry.init({
       dsn: PUBLIC_SENTRY_DSN,
     });
@@ -14,7 +14,7 @@ export const setupSentry = (): void => {
 export const matomoClientInstance = createInstance({
   urlBase: MATOMO_BASE_URL, // include trailing slash
   siteId: parseInt(MATOMO_SITE_ID, 10),
-  disabled: !MATOMO_BASE_URL || !MATOMO_SITE_ID,
+  disabled: isEnvVarEmpty('MATOMO_BASE_URL') || isEnvVarEmpty('MATOMO_SITE_ID'),
   configurations: {
     disableCookies: true,
   },
