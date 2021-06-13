724941285072d9d76f3635a90f4f29d3c93b6334
Michał Oręziak <micorix.dev@gmail.com>
2021-06-13 20:45:44 +0200

    Remove API_URL env var

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,27 +1,24 @@
 export interface LocalEnvironment {
-  REACT_APP_API_URL: string;
+  REACT_APP_GRAPHQL_ENDPOINT: string;
   REACT_APP_PUBLIC_SENTRY_DSN: string;
   REACT_APP_MATOMO_BASE_URL: string;
   REACT_APP_MATOMO_SITE_ID: string;
-  REACT_APP_GRAPHQL_ENDPOINT: string;
 }
 
 export interface Environment {
-  API_URL: string;
+  GRAPHQL_ENDPOINT: string;
   PUBLIC_SENTRY_DSN: string;
   MATOMO_BASE_URL: string;
   MATOMO_SITE_ID: string;
-  GRAPHQL_ENDPOINT: string;
 }
 
 const prodEnvironment: Environment = window.config;
 
 const devEnvironment: Environment = {
-  API_URL: process.env.REACT_APP_API_URL,
-  PUBLIC_SENTRY_DSN: process.env.REACT_APP_API_URL,
+  GRAPHQL_ENDPOINT: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+  PUBLIC_SENTRY_DSN: process.env.REACT_APP_PUBLIC_SENTRY_DSN,
   MATOMO_BASE_URL: process.env.REACT_APP_MATOMO_BASE_URL,
   MATOMO_SITE_ID: process.env.REACT_APP_MATOMO_SITE_ID,
-  GRAPHQL_ENDPOINT: process.env.REACT_APP_GRAPHQL_ENDPOINT,
 };
 
 export const isProduction = process.env.NODE_ENV === 'production';
