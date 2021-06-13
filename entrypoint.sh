80e926930f3ca483223e0bfe5f324fbe35d733fa
Michał Oręziak <micorix.dev@gmail.com>
2021-06-13 14:37:47 +0200

    Add GRAPHQL_ENDPOINT env var to index.html & entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ envsubst < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/app.conf
 
 # Non-expanded expressions (single quotes) on purpose.
 envsubst '
+${GRAPHQL_ENDPOINT}
 ${API_URL}
 ${PUBLIC_SENTRY_DSN}
 ${MATOMO_BASE_URL}
