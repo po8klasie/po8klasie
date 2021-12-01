0861099a5101a1ad6a1566d9e62f76d2793b799c
Michał Oręziak <micorix.dev@gmail.com>
2021-12-01 14:33:58 +0100

    Add tests for config/nextHelpers (#143)

---
+++ src/config/tests/nextHelpers.spec.ts
@@ -0,0 +1,56 @@
+import * as projectConfigIndex from '../index';
+import { getProjectConfigStaticProps, getStaticPathsPerProject } from '../nextHelpers';
+import appearance from '../warszawa/appearance';
+
+describe('config/nextHelpers', () => {
+  describe('getProjectConfigStaticProps', () => {
+    const fakeConfig = {
+      filters: { someFilter: 'filter' },
+      appearance: { someProp: false },
+    };
+
+    it('calls getProjectConfig with proper projectID', async () => {
+      const spy = jest
+        .spyOn(projectConfigIndex, 'getProjectConfig')
+        .mockImplementation(() => fakeConfig as any);
+
+      await getProjectConfigStaticProps(['appearance'])({ params: { projectID: 'foo' } });
+      expect(spy).toHaveBeenCalledWith('foo');
+    });
+
+    it('returns props for project config', async () => {
+      projectConfigIndex.getProjectConfig = jest.fn(() => fakeConfig);
+
+      const result = await getProjectConfigStaticProps(['appearance'])({
+        params: { projectID: 'bar' },
+      });
+      
+      expect(result).toEqual({
+        props: {
+          PROJECT: {
+            appearance: fakeConfig.appearance,
+          },
+        },
+      });
+    });
+  });
+  describe('getStaticPathsPerProject', () => {
+    it('returns proper paths', async () => {
+      projectConfigIndex.projectsIDs = ['abc', 'foo'];
+
+      const result = await getStaticPathsPerProject();
+      expect(result.paths).toEqual([
+        {
+          params: {
+            projectID: 'abc',
+          },
+        },
+        {
+          params: {
+            projectID: 'foo',
+          },
+        },
+      ]);
+    });
+  });
+});
