86db202386701158e0dc625c484509008385a234
Michał Oręziak <micorix.dev@gmail.com>
2021-12-05 15:02:18 +0100

    Serve landing page as index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,40 @@
-const LandingPage = () => <h1>Hello world</h1>
+import React, { FC } from 'react';
+import Head from 'next/head';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'tailwindcss/tailwind.css';
+
+import HeroSection from '../components/website/LandingPage/HeroSection';
+import Layout from '../components/website/Layout';
+import PartnersSection from '../components/website/LandingPage/PartnersSection';
+import ContactUsSection from '../components/website/LandingPage/ContactUsSection';
+import WhatDoWeDo from '../components/website/LandingPage/WhatDoWeDo';
+import CivicTechSection from '../components/website/LandingPage/CivicTechSection';
+import FeaturesSection from '../components/website/LandingPage/FeaturesSection';
+import TeamSection from '../components/website/LandingPage/TeamSection';
+import OurStorySection from '../components/website/LandingPage/OurStorySection';
+import SupportUsSection from '../components/website/LandingPage/SupportUsSection';
+
+const LandingPage: FC = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>po8klasie - wyszukiwarka szkół średnich</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <HeroSection />
+      <WhatDoWeDo />
+      <CivicTechSection />
+      <FeaturesSection />
+      <OurStorySection />
+      <TeamSection />
+      <PartnersSection />
+      <SupportUsSection />
+      <ContactUsSection />
+    </Layout>
+  );
+};
 
 export default LandingPage;
