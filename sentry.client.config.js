9239d2c5dcb16c0c5c8b56e5781d891cf8fcc843
Michał Oręziak <micorix.dev@gmail.com>
2021-12-05 16:24:17 +0100

    Add Sentry and Matomo to next

---
+++ sentry.client.config.js
@@ -0,0 +1,12 @@
+import * as Sentry from '@sentry/nextjs';
+import { environment, isEnvVarEmpty } from "src/environments/environment";
+
+const SENTRY_DSN = environment.PUBLIC_SENTRY_DSN;
+
+if(!isEnvVarEmpty('PUBLIC_SENTRY_DSN'))
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    tracesSampleRate: 1.0,
+  })
+
+
