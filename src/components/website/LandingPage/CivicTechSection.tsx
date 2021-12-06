e2420559a7fc9025391d4c694de87e15c220c723
Michał Oręziak <micorix.dev@gmail.com>
2021-12-06 19:29:59 +0100

    Add new content

--- src/components/website/LandingPage/CivicTechSection.tsx
+++ src/components/website/LandingPage/CivicTechSection.tsx
@@ -6,10 +6,10 @@ const CivicTechSection: FC = () => (
     <div className="w-narrowContainer mx-auto">
       <h2 className="text-center text-3xl font-bold">Technologia obywatelska</h2>
       <p className="mt-10 text-center text-lg">
-        <Brand/> tworzymy jako projekt <strong>civic-tech</strong>. Ruch civic tech (technologii obywatelskich) to forma aktywizmu społecznego,
-        która skupia się na zastosowaniu technologii do poprawienia współpracy między obywatelami a samorządami.
-        Inicjatywy civic-tech często, tak jak w naszym przypadku, mają swoje źródło w konkretnych problemach,
-        które motywują obywateli do próby rozwiązania ich we współpracy z urzędem.
+        <Brand /> tworzymy jako projekt <strong>civic tech</strong>. Ruch civic tech (technologii obywatelskich) to forma aktywizmu społecznego,
+        która skupia się na stosowaniu technologii do wzmocnienia i poprawy współpracy między obywatelami a administracją publiczną.
+        Inicjatywy civic tech mają swoje źródło w konkretnych problemach społecznych dotyczących zwykłych ludzi,
+        a najważniejszym celem jest nie sprzedaż rozwiązania, ale próba rozwiązania kwestii, które są problemem.
       </p>
     </div>
   </div>
