6df2e2f7ae5f9df1e46bbe539cc5ce7e7a834602
Michał Oręziak <micorix.dev@gmail.com>
2021-12-06 19:40:15 +0100

    Replace CivicTechSection content (#157)
    * Replace CivicTechSection content
    
    * Add new content

--- src/components/website/LandingPage/CivicTechSection.tsx
+++ src/components/website/LandingPage/CivicTechSection.tsx
@@ -6,9 +6,10 @@ const CivicTechSection: FC = () => (
     <div className="w-narrowContainer mx-auto">
       <h2 className="text-center text-3xl font-bold">Technologia obywatelska</h2>
       <p className="mt-10 text-center text-lg">
-        <Brand /> tworzymy jako projekt <strong>civic-tech</strong>. W języku polskim, tłumaczymy to
-        jako “technologie obywatelskie”. Oznacza to, iż najważniejsze jest dla nas rozwiązanie problemu,
-        czyli ułatwienie młodzieży wybóru szkoły.
+        <Brand /> tworzymy jako projekt <strong>civic tech</strong>. Ruch civic tech (technologii obywatelskich) to forma aktywizmu społecznego,
+        która skupia się na stosowaniu technologii do wzmocnienia i poprawy współpracy między obywatelami a administracją publiczną.
+        Inicjatywy civic tech mają swoje źródło w konkretnych problemach społecznych dotyczących zwykłych ludzi,
+        a najważniejszym celem jest nie sprzedaż rozwiązania, ale próba rozwiązania kwestii, które są problemem.
       </p>
     </div>
   </div>
--- src/components/website/LandingPage/OurStorySection.tsx
+++ src/components/website/LandingPage/OurStorySection.tsx
@@ -9,7 +9,7 @@ const OurStorySection: FC = () => (
       <h2 className="text-center text-3xl font-bold">Historia</h2>
       <p className="mt-10 text-center text-lg">
         Michał, który zainicjował projkt sam stanął przed problem rozproszonych informacji o
-        szkołach średnich podczas swojej rekturacji. Postanowił rozwiązać ten problem. Jendocześnie
+        szkołach średnich podczas swojej rekrutacji. Postanowił rozwiązać ten problem. Jendocześnie
         ucząc się programowania, tworzył z wszystkich wyszukanych informacji bazę danych
         warszawskich liceów. Tym projektem wciągnął innych fascynatów. Wiedziliśmy, że nie możemy
         ograniczać się do liceów.
