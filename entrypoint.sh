c6860b6d33475fa24a370c36dce702659e3fe811
Michał Oręziak <micorix.dev@gmail.com>
2022-01-02 18:07:35 +0100

    Envsubst based on file pattern. Add Sentry tracing. (#162)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,30 @@
-#!/usr/bin/ash
+#!/usr/bin/bash
 
 set -e
 
 # Non-expanded expressions (single quotes) on purpose.
-envsubst '
+vars_to_replace_in_nginx_conf='
 ${NGINX_SERVER_NAME}
 ${NGINX_BACK_URL}
-'< /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/app.conf
+'
 
-envsubst '
+vars_to_replace_in_html='
 ${GRAPHQL_ENDPOINT}
 ${PUBLIC_SENTRY_DSN}
 ${MATOMO_BASE_URL}
 ${MATOMO_SITE_ID}
-' < /opt/nginx/out/warszawa/legacy.html > /opt/nginx/out/warszawa/legacy.html.subst
+${APP_ENVIRONMENT}
+${APP_RELEASE}
+'
 
-cp -pf /opt/nginx/out/warszawa/legacy.html.subst /opt/nginx/out/warszawa/legacy.html
+
+envsubst "$vars_to_replace_in_nginx_conf" < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/app.conf
+
+shopt -s nullglob globstar
+
+for html_file in /opt/nginx/**/*.html; do
+  envsubst "$vars_to_replace_in_html" < "$html_file" > "$html_file.subst"
+  mv "$html_file.subst" "$html_file"
+done
 
 exec nginx -g 'daemon off;'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export interface LocalEnvironment {
   REACT_APP_GRAPHQL_ENDPOINT: string;
+  REACT_APP_APP_ENVIRONMENT: string;
+  REACT_APP_APP_FRONTEND_RELEASE: string;
   REACT_APP_PUBLIC_SENTRY_DSN: string;
   REACT_APP_MATOMO_BASE_URL: string;
   REACT_APP_MATOMO_SITE_ID: string;
@@ -7,6 +9,8 @@ export interface LocalEnvironment {
 
 export interface Environment {
   GRAPHQL_ENDPOINT: string;
+  APP_ENVIRONMENT: string;
+  APP_FRONTEND_RELEASE: string;
   PUBLIC_SENTRY_DSN: string;
   MATOMO_BASE_URL: string;
   MATOMO_SITE_ID: string;
@@ -16,6 +20,8 @@ const prodEnvironment: Environment = window.config;
 
 const devEnvironment: Environment = {
   GRAPHQL_ENDPOINT: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+  APP_FRONTEND_RELEASE: process.env.REACT_APP_APP_FRONTEND_RELEASE,
+  APP_ENVIRONMENT: process.env.REACT_APP_APP_ENVIRONMENT,
   PUBLIC_SENTRY_DSN: process.env.REACT_APP_PUBLIC_SENTRY_DSN,
   MATOMO_BASE_URL: process.env.REACT_APP_MATOMO_BASE_URL,
   MATOMO_SITE_ID: process.env.REACT_APP_MATOMO_SITE_ID,
@@ -27,3 +33,6 @@ export const environment: Environment = isProduction ? prodEnvironment : devEnvi
 
 export const isEnvVarEmpty = (name: keyof Environment): boolean =>
   !environment[name] || environment[name] === `\${${name}}`;
+
+export const isAnyEnvVarEmpty = (names: (keyof Environment)[]): boolean =>
+  names.some(isEnvVarEmpty);
