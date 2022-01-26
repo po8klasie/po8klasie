3ad15868c0138240bcff1d6c97532ec49cc2b8d8
Michał Oręziak <micorix.dev@gmail.com>
2022-01-26 22:41:07 +0100

    Fix calculator reset for good

--- src/components/calculator/CalculatorInputs.tsx
+++ src/components/calculator/CalculatorInputs.tsx
@@ -1,5 +1,5 @@
 import { useFormContext } from 'react-hook-form';
-import { ChangeEvent, FC, HTMLProps, KeyboardEvent, useState } from 'react';
+import { ChangeEvent, FC, HTMLProps, KeyboardEvent } from 'react';
 import { isExamResultValid, isGradeValid } from '@warsawlo/points-calculator/lib/utils/validators';
 
 export interface CalculatorInputProps extends HTMLProps<HTMLInputElement> {
@@ -7,24 +7,22 @@ export interface CalculatorInputProps extends HTMLProps<HTMLInputElement> {
 }
 
 export const GradeInput: FC<CalculatorInputProps> = ({ inputId, ...props }) => {
-  const { register, setValue: setValueOriginal, setError } = useFormContext();
-  const r = register(inputId);
-  const setValue = (value: unknown) => setValueOriginal(inputId, value);
-  const [internalValue, setInternalValue] = useState('');
+  const { watch, setValue: setValueOriginal, setError } = useFormContext();
+  const value = watch(inputId);
+  const setValue = (updatedValue: number | null) => setValueOriginal(inputId, updatedValue);
 
   const handleKeyDown = (e: KeyboardEvent) => {
-    e.preventDefault();
     if (['Backspace', 'Delete'].includes(e.key)) {
-      setInternalValue('');
+      e.preventDefault();
       setValue(null);
-      return;
     }
-    if (e.key.length > 1) return;
+  };
 
-    setInternalValue(e.key);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const updatedValue = e.target.value.at(-1);
 
-    const parsedKey = parseInt(e.key, 10);
-    if (isGradeValid(parsedKey)) setValue(parsedKey);
+    const parsedValue = parseInt(updatedValue, 10);
+    if (isGradeValid(parsedValue)) setValue(parsedValue);
     else {
       setValue(null);
       setError(inputId, { message: 'Invalid value' });
@@ -33,30 +31,25 @@ export const GradeInput: FC<CalculatorInputProps> = ({ inputId, ...props }) => {
 
   const inputProps: Partial<HTMLProps<HTMLInputElement>> = {
     ...props,
-    ...r,
     placeholder: '5',
     'aria-label': 'Ocena',
     inputMode: 'numeric',
-    value: internalValue,
+    value: value ?? '',
     onKeyDown: handleKeyDown,
+    onChange: handleChange,
+    autoComplete: 'off',
   };
 
   return <input {...inputProps} data-testid="input" />;
 };
 
 export const ExamResultInput: FC<CalculatorInputProps> = ({ inputId, ...props }) => {
-  const { setValue: setValueOriginal, setError } = useFormContext();
-  const setValue = (value: unknown) => setValueOriginal(inputId, value);
-  const [internalValue, setInternalValue] = useState('');
+  const { watch, setValue: setValueOriginal, setError } = useFormContext();
+  const value = watch(inputId);
+  const setValue = (updatedValue: number | null) => setValueOriginal(inputId, updatedValue);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-
-    if (value.length <= 3) {
-      setInternalValue(value);
-    }
-
-    const score = parseInt(value, 10) / 100;
+    const score = parseInt(e.target.value, 10) / 100;
     if (isExamResultValid(score)) {
       setValue(score);
     } else {
@@ -69,7 +62,7 @@ export const ExamResultInput: FC<CalculatorInputProps> = ({ inputId, ...props })
     <input
       {...props}
       onChange={handleChange}
-      value={internalValue}
+      value={value ? value * 100 : ''}
       inputMode="decimal"
       maxLength={3}
       placeholder="%"
--- src/components/calculator/tests/CalculatorInputs.spec.tsx
+++ src/components/calculator/tests/CalculatorInputs.spec.tsx
@@ -1,37 +1,54 @@
 import { fireEvent, render } from '@testing-library/react';
 import * as reactHookForm from 'react-hook-form';
+import { useEffect, useRef, useState } from 'react';
 import { ExamResultInput, GradeInput } from '../CalculatorInputs';
 
-const useFormContextMockedReturn = {
-  register: () => ({
-    onChange: jest.fn(),
-  }),
+let useFormContextMockedFunctions = {
   setValue: jest.fn(),
   setError: jest.fn(),
 };
 
 beforeEach(() => {
-  reactHookForm.useFormContext = () => useFormContextMockedReturn;
+  reactHookForm.useFormContext = jest.fn().mockImplementation(() => {
+    const [value, setValue] = useState('');
+    const ref = useRef({
+      setValue: jest.fn().mockImplementation((_, v) => setValue(v)),
+      setError: jest.fn(),
+    });
+
+    useEffect(() => {
+      useFormContextMockedFunctions = ref.current;
+    }, []);
+
+    return {
+      setValue: ref.current.setValue,
+      setError: ref.current.setError,
+      watch: () => value,
+    };
+  });
 });
 
 afterEach(() => {
   jest.clearAllMocks();
+  useFormContextMockedFunctions = {
+    setValue: jest.fn(),
+    setError: jest.fn(),
+  };
 });
 
 describe('components/calculator/CalculatorInputs', () => {
   describe('GradeInput', () => {
-    it(`accepts one char`, () => {
+    it(`accepts last char`, () => {
       const wrapper = render(<GradeInput inputId="foo" />);
       const input = wrapper.getByTestId('input');
-      fireEvent.keyDown(input, { key: 'a' });
-      fireEvent.keyDown(input, { key: 'b' });
-      expect(input).toHaveValue('b');
+      fireEvent.change(input, { target: { value: '2' } });
+      expect(input).toHaveValue('2');
     });
 
     it(`clears on Backspace`, () => {
       const wrapper = render(<GradeInput inputId="foo" />);
       const input = wrapper.getByTestId('input');
-      fireEvent.keyDown(input, { key: 'a' });
+      fireEvent.change(input, { target: { value: '4' } });
       fireEvent.keyDown(input, { key: 'Backspace' });
       expect(input).toHaveValue('');
     });
@@ -39,7 +56,7 @@ describe('components/calculator/CalculatorInputs', () => {
     it(`clears on Delete`, () => {
       const wrapper = render(<GradeInput inputId="foo" />);
       const input = wrapper.getByTestId('input');
-      fireEvent.keyDown(input, { key: 'a' });
+      fireEvent.change(input, { target: { value: 'a' } });
       fireEvent.keyDown(input, { key: 'Delete' });
       expect(input).toHaveValue('');
     });
@@ -48,43 +65,43 @@ describe('components/calculator/CalculatorInputs', () => {
       const wrapper = render(<GradeInput inputId="foo" />);
       const input = wrapper.getByTestId('input');
       fireEvent.keyDown(input, { key: 'Backspace' });
-      expect(useFormContextMockedReturn.setValue).toHaveBeenCalledWith('foo', null);
+      expect(useFormContextMockedFunctions.setValue).toHaveBeenCalledWith('foo', null);
     });
 
     it(`calls setValue with arg null on Delete`, () => {
       const wrapper = render(<GradeInput inputId="foo" />);
       const input = wrapper.getByTestId('input');
       fireEvent.keyDown(input, { key: 'Delete' });
-      expect(useFormContextMockedReturn.setValue).toHaveBeenCalledWith('foo', null);
+      expect(useFormContextMockedFunctions.setValue).toHaveBeenCalledWith('foo', null);
     });
 
     it(`preserves value when key different than 1..6, Delete, Backspace was pressed`, () => {
       const wrapper = render(<GradeInput inputId="foo" />);
       const input = wrapper.getByTestId('input');
-      fireEvent.keyDown(input, { key: 'c' });
+      fireEvent.change(input, { target: { value: '2' } });
       fireEvent.keyDown(input, { key: 'ArrowLeft' });
-      expect(input).toHaveValue('c');
+      expect(input).toHaveValue('2');
     });
 
     it(`does not call setValue when key different than 1..6, Delete, Backspace was pressed`, () => {
       const wrapper = render(<GradeInput inputId="foo" />);
       const input = wrapper.getByTestId('input');
       fireEvent.keyDown(input, { key: 'ArrowLeft' });
-      expect(useFormContextMockedReturn.setValue).not.toHaveBeenCalled();
+      expect(useFormContextMockedFunctions.setValue).not.toHaveBeenCalled();
     });
 
     it(`calls setValue with arg null when invalid value was entered`, () => {
       const wrapper = render(<GradeInput inputId="bar" />);
       const input = wrapper.getByTestId('input');
-      fireEvent.keyDown(input, { key: 'g' });
-      expect(useFormContextMockedReturn.setValue).toHaveBeenCalledWith('bar', null);
+      fireEvent.change(input, { target: { value: 'c' } });
+      expect(useFormContextMockedFunctions.setValue).toHaveBeenCalledWith('bar', null);
     });
 
     it(`calls setError when invalid value was entered`, () => {
       const wrapper = render(<GradeInput inputId="bar" />);
       const input = wrapper.getByTestId('input');
-      fireEvent.keyDown(input, { key: 'g' });
-      expect(useFormContextMockedReturn.setError).toHaveBeenCalledWith('bar', {
+      fireEvent.change(input, { target: { value: 'g' } });
+      expect(useFormContextMockedFunctions.setError).toHaveBeenCalledWith('bar', {
         message: 'Invalid value',
       });
     });
@@ -95,31 +112,31 @@ describe('components/calculator/CalculatorInputs', () => {
       it(`calls setValue when valid value (${grade}) was entered`, () => {
         const wrapper = render(<GradeInput inputId="bar" />);
         const input = wrapper.getByTestId('input');
-        fireEvent.keyDown(input, { key: grade });
-        expect(useFormContextMockedReturn.setValue).toHaveBeenCalledWith('bar', grade);
+        fireEvent.change(input, { target: { value: grade } });
+        expect(useFormContextMockedFunctions.setValue).toHaveBeenCalledWith('bar', grade);
       });
 
       it(`does not call setError when valid value (${grade}) was entered`, () => {
         const wrapper = render(<GradeInput inputId="bar" />);
         const input = wrapper.getByTestId('input');
-        fireEvent.keyDown(input, { key: grade });
-        expect(useFormContextMockedReturn.setError).not.toHaveBeenCalled();
+        fireEvent.change(input, { target: { value: grade } });
+        expect(useFormContextMockedFunctions.setError).not.toHaveBeenCalled();
       });
     });
   });
 
   describe('ExamResultInput', () => {
-    it(`doesn't accept up to 3 chars`, () => {
+    it(`accepts number up to 2 digits`, () => {
       const wrapper = render(<ExamResultInput inputId="fizz" />);
       const input = wrapper.getByTestId('input');
-      fireEvent.change(input, { target: { value: 'abc' } });
-      expect(input).toHaveValue('abc');
+      fireEvent.change(input, { target: { value: '99' } });
+      expect(input).toHaveValue('99');
     });
 
-    it(`doesn't accept > 3 chars`, () => {
+    it(`doesn't accept > 100`, () => {
       const wrapper = render(<ExamResultInput inputId="fizz" />);
       const input = wrapper.getByTestId('input');
-      fireEvent.change(input, { target: { value: 'abcd' } });
+      fireEvent.change(input, { target: { value: '400' } });
       expect(input).toHaveValue('');
     });
 
@@ -127,20 +144,20 @@ describe('components/calculator/CalculatorInputs', () => {
       const wrapper = render(<ExamResultInput inputId="fizz" />);
       const input = wrapper.getByTestId('input');
       fireEvent.change(input, { target: { value: '87' } });
-      expect(useFormContextMockedReturn.setValue).toHaveBeenCalledWith('fizz', 0.87);
+      expect(useFormContextMockedFunctions.setValue).toHaveBeenCalledWith('fizz', 0.87);
     });
     it(`calls setValue with arg null when value is invalid`, () => {
       const wrapper = render(<ExamResultInput inputId="fizz" />);
       const input = wrapper.getByTestId('input');
       fireEvent.change(input, { target: { value: '103' } });
-      expect(useFormContextMockedReturn.setValue).toHaveBeenCalledWith('fizz', null);
+      expect(useFormContextMockedFunctions.setValue).toHaveBeenCalledWith('fizz', null);
     });
 
     it(`calls setError when invalid value was entered`, () => {
       const wrapper = render(<ExamResultInput inputId="fizz" />);
       const input = wrapper.getByTestId('input');
       fireEvent.change(input, { target: { value: '-21' } });
-      expect(useFormContextMockedReturn.setError).toHaveBeenCalledWith('fizz', {
+      expect(useFormContextMockedFunctions.setError).toHaveBeenCalledWith('fizz', {
         message: 'Invalid value',
       });
     });
@@ -149,7 +166,7 @@ describe('components/calculator/CalculatorInputs', () => {
       const wrapper = render(<ExamResultInput inputId="fizz" />);
       const input = wrapper.getByTestId('input');
       fireEvent.change(input, { target: { value: '12' } });
-      expect(useFormContextMockedReturn.setError).not.toHaveBeenCalled();
+      expect(useFormContextMockedFunctions.setError).not.toHaveBeenCalled();
     });
   });
 });
