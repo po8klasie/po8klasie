797530e13a61e29a703f9fb14dc783a93d7f2d72
Michał Oręziak <micorix.dev@gmail.com>
2022-02-07 01:05:23 +0100

    Add next-seo (#173)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import { DefaultSeo } from '../Seo';
 import 'normalize.css';
 import '../styling/global.css';
 
@@ -13,9 +14,9 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>po8klasie</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <DefaultSeo />
       <NextMatomoInitalizer />
       <Component {...pageProps} />
     </>
