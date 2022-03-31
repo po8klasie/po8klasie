661227457b249fbb36c834282c018069cd5f11fa
Michał Oręziak <micorix.dev@gmail.com>
2022-03-31 14:08:09 +0200

    Replace switch with object mapping

--- src/api/railsAPI/schools.ts
+++ src/api/railsAPI/schools.ts
@@ -1,31 +1,26 @@
 import useSWR from 'swr';
 import qs, { StringifiableRecord } from 'query-string';
-import camelCaseKeys from 'camelcase-keys';
 import { isPlainObject, camelCase } from 'lodash';
 
-const keysToCamel = function (o: Record<string, unknown>) {
+const camelCaseKeys = (o: Record<string, unknown>): Record<string, unknown> | unknown[] => {
   if (isPlainObject(o)) {
     const n: Record<string, unknown> = {};
 
     Object.keys(o).forEach((k) => {
-      n[camelCase(k)] = keysToCamel(o[k] as Record<string, unknown>);
+      n[camelCase(k)] = camelCaseKeys(o[k] as Record<string, unknown>);
     });
 
     return n;
   } else if (Array.isArray(o)) {
-    return o.map((i) => {
-      return keysToCamel(i);
-    });
+    return o.map((i) => camelCaseKeys(i));
   }
 
   return o;
 };
 
-const fetcher = async (url: string) => {
-  const res = await fetch(url).then((res) => res.json());
-  console.log(res, keysToCamel(res));
-  return keysToCamel(res);
-};
+const fetcher = (url: string) => fetch(url)
+  .then((res) => res.json())
+  .then(camelCaseKeys)
 
 const useSchoolsData = (filtersValues: StringifiableRecord, defaultQuery: StringifiableRecord) => {
   const endpointUrl = qs.stringifyUrl(
