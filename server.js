de6791e5f38e52625499832e0b18ce1db2cab9b3
Michał Oręziak <micorix.dev@gmail.com>
2022-04-04 15:52:21 +0200

    Use custom server

---
+++ server.js
@@ -0,0 +1,30 @@
+//  https://nextjs.org/docs/advanced-features/custom-server
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const conf = require('./next.config.js');
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = process.env.HOSTNAME ?? 'localhost';
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port, conf });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err);
+      res.statusCode = 500;
+      res.end('internal server error');
+    }
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
