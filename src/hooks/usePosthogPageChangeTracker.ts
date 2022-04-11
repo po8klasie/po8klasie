92c9159e5e3e8a74728642fc6711f5ae83fce6e1
Michał Oręziak <micorix.dev@gmail.com>
2022-04-11 17:15:31 +0200

    Intergrate Posthog analytics

---
+++ src/hooks/usePosthogPageChangeTracker.ts
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import posthog from 'posthog-js';
+
+const posthogApiHost = 'https://app.posthog.com'
+
+// https://posthog.com/docs/integrate/third-party/next-js
+const usePosthogPageChangeTracker = () => {
+  const router = useRouter();
+
+  const { POSTHOG_API_KEY } = publicRuntimeConfig;
+
+  useEffect(() => {
+    if(POSTHOG_API_KEY) {
+      posthog.init(POSTHOG_API_KEY, { api_host: posthogApiHost });
+
+      const handleRouteChange = () => posthog.capture('$pageview');
+      router.events.on('routeChangeComplete', handleRouteChange);
+
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange);
+      }
+    }
+
+    return () => {}
+  }, []);
+}
+
+export default usePosthogPageChangeTracker;
